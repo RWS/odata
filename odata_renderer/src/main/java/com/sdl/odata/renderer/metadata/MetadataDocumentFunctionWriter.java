bee2f4e60a2dff9f339716c7189e5fac63fa203d
Thomas Matecki <thomas.matecki@apache.org>
2016-12-13 09:31:45 -0500

    Cleanup for checkstyle

--- odata_renderer/src/main/java/com/sdl/odata/renderer/metadata/MetadataDocumentFunctionWriter.java
+++ odata_renderer/src/main/java/com/sdl/odata/renderer/metadata/MetadataDocumentFunctionWriter.java
@@ -8,9 +8,24 @@ import org.slf4j.LoggerFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
-import static com.sdl.odata.MetadataDocumentConstants.*;
+import static com.sdl.odata.MetadataDocumentConstants.FUNCTION;
+import static com.sdl.odata.MetadataDocumentConstants.NAME;
+import static com.sdl.odata.MetadataDocumentConstants.PARAMETER;
+import static com.sdl.odata.MetadataDocumentConstants.TYPE;
+import static com.sdl.odata.MetadataDocumentConstants.NULLABLE;
+import static com.sdl.odata.MetadataDocumentConstants.RETURN_TYPE;
+
 import static com.sdl.odata.ODataRendererUtils.checkNotNull;
 
+/**
+ * <p>
+ * Helper writer capable of writing  {@code <Function>} elements.
+ * </p>
+ * <p>
+ * Please note that it is necessary to open the XML writer used by instances of this class before calling any method,
+ * and close it after the writing process is finished.
+ * </p>
+ */
 public class MetadataDocumentFunctionWriter {
 
     private static final Logger LOG = LoggerFactory.getLogger(MetadataDocumentSingletonWriter.class);
@@ -25,28 +40,25 @@ public class MetadataDocumentFunctionWriter {
         this.xmlWriter = checkNotNull(xmlWriter);
     }
 
-    public void write(Function function) {
+    public void write(Function function) throws XMLStreamException {
 
-        try {
-            xmlWriter.writeStartElement(FUNCTION);
-            xmlWriter.writeAttribute(NAME, function.getName());
+        xmlWriter.writeStartElement(FUNCTION);
+        xmlWriter.writeAttribute(NAME, function.getName());
 
-            for (Parameter parameter : function.getParameters()) {
-                xmlWriter.writeStartElement(PARAMETER);
-                xmlWriter.writeAttribute(NAME, parameter.getName());
-                xmlWriter.writeAttribute(TYPE, parameter.getType());
-                xmlWriter.writeAttribute(NULLABLE, Boolean.toString(parameter.isNullable()));
-                xmlWriter.writeEndElement();
-            }
-
-            xmlWriter.writeStartElement(RETURN_TYPE);
-            xmlWriter.writeAttribute(TYPE, function.getReturnType());
+        for (Parameter parameter : function.getParameters()) {
+            xmlWriter.writeStartElement(PARAMETER);
+            xmlWriter.writeAttribute(NAME, parameter.getName());
+            xmlWriter.writeAttribute(TYPE, parameter.getType());
+            xmlWriter.writeAttribute(NULLABLE, Boolean.toString(parameter.isNullable()));
             xmlWriter.writeEndElement();
-
-            xmlWriter.writeEndElement();
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
         }
+
+        xmlWriter.writeStartElement(RETURN_TYPE);
+        xmlWriter.writeAttribute(TYPE, function.getReturnType());
+        xmlWriter.writeEndElement();
+
+        xmlWriter.writeEndElement();
+
     }
 
 }
