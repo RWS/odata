d3878258e85192d6692add78e9a0d927c8ee8cda
Ben Middleton <bmiddleton@sdl.com>
2020-04-02 12:09:49 +0100

    Revert "Merge branch 'release/2.6' into release/2.5"
    This reverts commit 69400f1a55ebe1295cce2f62fcac765031dbdc95, reversing
    changes made to 652cde4e23be702569eb1fd7e70bea52510bea8f.

--- odata_renderer/src/test/java/com/sdl/odata/unmarshaller/batch/BatchUnmarshallerTest.java
+++ odata_renderer/src/test/java/com/sdl/odata/unmarshaller/batch/BatchUnmarshallerTest.java
@@ -28,7 +28,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
 
