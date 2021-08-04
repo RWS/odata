b647767875d417bd767b715a0e4d693feb21b441
inikonov <inikonov@sdl.com>
2021-08-04 15:20:25 +0300

    UDP-9866 SDL to RWS

--- odata_renderer/src/main/java/com/sdl/odata/unmarshaller/atom/ODataAtomParser.java
+++ odata_renderer/src/main/java/com/sdl/odata/unmarshaller/atom/ODataAtomParser.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014 All Rights Reserved by the SDL Group.
+ * Copyright (c) 2014 All Rights Reserved by the RWS Group.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
