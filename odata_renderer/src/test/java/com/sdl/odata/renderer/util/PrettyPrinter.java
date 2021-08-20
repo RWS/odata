407bbd94b33a802832ea26edf3dd944da9897dc0
Igor Nikonov <inikonov@sdl.com>
2021-08-20 13:48:47 +0300

    UDP-451 removed comments from xml in test reources

--- odata_renderer/src/test/java/com/sdl/odata/renderer/util/PrettyPrinter.java
+++ odata_renderer/src/test/java/com/sdl/odata/renderer/util/PrettyPrinter.java
@@ -20,7 +20,6 @@ import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
