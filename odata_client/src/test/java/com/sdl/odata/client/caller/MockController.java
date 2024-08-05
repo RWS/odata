ec1baff6f3747ab165a6521a72881ec2bab6428e
Maxim Chireychik <mchireychik@sdl.com>
2024-08-05 15:42:22 +0300

    UDP-11072 re-enable test

--- odata_client/src/test/java/com/sdl/odata/client/caller/MockController.java
+++ odata_client/src/test/java/com/sdl/odata/client/caller/MockController.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2014-2024 All Rights Reserved by the RWS Group for and on behalf of its affiliates and subsidiaries.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,14 @@
  */
 package com.sdl.odata.client.caller;
 
-import java.io.IOException;
-import java.io.InputStream;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.springframework.util.StreamUtils.copyToString;
@@ -29,14 +30,13 @@ import static org.springframework.util.StreamUtils.copyToString;
 /**
  * MockController.
  */
-@Component
-@RequestMapping("/")
+@Controller
 class MockController {
 
     private static final String RESPONSE = "response.xml";
 
     @RequestMapping(value = "{code:\\d{3}}")
-    ResponseEntity<?> respondWithCode(@PathVariable int code) throws IOException {
+    ResponseEntity<?> respondWithCode(@PathVariable("code") int code) throws IOException {
         return ResponseEntity.status(HttpStatus.valueOf(code)).body(String.valueOf(code));
     }
 
